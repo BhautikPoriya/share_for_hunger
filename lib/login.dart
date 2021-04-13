1e7ab5d5ac0ff90e164fe9eb54114d0c3a87868c
BhautikPoriya <bporiya2001@gmail.com>
2021-04-13 14:23:30 +0530

    Create New Login-Signup Ui

---
+++ lib/login.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        // Back Icon
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+          size: 20,
+          color: Colors.black,),
+        ),
+      ),
+
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Login",
+                    style: TextStyle(fontSize: 39, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 9,),
+                    Text("Login to your account",
+                    style: TextStyle(
+                      fontSize: 15,
+                    color:Colors.grey[700]),)
+                  ],
+                ),
+
+                SizedBox(height: 30),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: <Widget>[
+                      inputFile(label: "Email"),
+                      inputFile(label: "Password", obscureText: true)
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+
+                Padding(padding:
+                EdgeInsets.symmetric(horizontal: 40),
+                child: Container(
+                  padding: EdgeInsets.only(top: 0, left: 0),
+                    decoration:
+                        
+                      BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border
+                          (
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black),
+                          )
+                        ),
+                      
+                      child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () {},
+                        color: Colors.amber,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Text(
+                          "Login", style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+
+// we will be creating a widget for text field
+Widget inputFile({label, obscureText = false})
+{
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.w400,
+          color:Colors.black
+        ),
+
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      TextField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0,
+          horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.amber
+            ),
+
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.amber)
+          )
+        ),
+      ),
+      SizedBox(height: 10,)
+    ],
+  );
+}
